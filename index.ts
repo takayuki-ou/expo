baef4f0b61d1508ca065e5cb3e0d26d4c98e553a
Takayuki Sumimoto <50979734+takayuki-ou@users.noreply.github.com>
2025-08-14 07:10:04 +0000

    dev: install ESLint.

--- index.ts
+++ index.ts
@@ -1,4 +1,4 @@
-import { registerRootComponent } from 'expo';
+// import { registerRootComponent } from 'expo';
 
 // import App from './src/app';
 
@@ -6,3 +6,5 @@ import { registerRootComponent } from 'expo';
 // It also ensures that whether you load the app in Expo Go or in a native build,
 // the environment is set up appropriately
 // registerRootComponent(App);
+
+"hello, world"
